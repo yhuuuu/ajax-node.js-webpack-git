0fac2f490103f4a89bfddb322a5b8593f77c8f3e
Yuting Hu <yuting.hu.careers@gmail.com>
2025-05-14 01:23:42 -0700

    Feature: Add book to the creator book list

--- AJAX-Day02-library-mangment-sys/js/index.js
+++ AJAX-Day02-library-mangment-sys/js/index.js
@@ -1,4 +1,8 @@
-// 1. Get creator's book list
+/**
+ * 1. Get creator's book list 
+ * 1.1 Fetch data from server
+ * 1.2 Render the book list into the table
+ * */
 const creator = 'cooper'
 const creatorList = document.querySelector('.table .list')
 // 1.1 Fetch data from server
@@ -30,3 +34,35 @@ const getBookList = function () {
 }
 // Fetch and render the list when the page has loaded
 getBookList()
+
+
+/**
+ * 2. Post a new book
+ * 2.1 Add button -> show and hide
+ * 2.2 Collect form data, and sumbit to the server
+ * 2.3 Re-render book list
+ */
+
+// 2.1 Create modal object 
+const addModalDom = document.querySelector('.add-modal')
+const addModal = new bootstrap.Modal(addModalDom)
+
+// 2.2 Collect form data
+document.querySelector('.modal-footer .add-btn').addEventListener('click', () => {
+    const addForm = document.querySelector('.add-form')
+    const bookObj = serialize(addForm, { hash: true, empty: true })
+   
+    axios.post('http://hmajax.itheima.net/api/books', {
+        ...bookObj,
+        creator
+    }).then(response => {
+        // 2.3 Re-render book list
+        getBookList()
+        // Clear form input
+        addForm.reset()
+        // Hide pop-up window
+        addModal.hide()
+    })
+})
+
+
