57a36dae8373fb067d1a2c8f8f9ca07c0587e07d
Yuting Hu <yuting.hu.careers@gmail.com>
2025-05-17 15:53:28 -0700

    added alear feature for edit confirmation

--- AJAX-Day02-library-mangment-sys/04.Account-setting/js/index.js
+++ AJAX-Day02-library-mangment-sys/04.Account-setting/js/index.js
@@ -8,14 +8,13 @@
 const creator = 'cooper'
 
 function renderPage() {
-    console.log('hi');
     axios(`http://hmajax.itheima.net/api/settings`, {
         params: {
             creator // Send creator as a query parameter
         }
     })
         .then((result) => {
-
+            const userObj = result.data.data
             // Expected fields:['avatar', 'nickname', 'email', 'desc', 'gender']
             Object.keys(userObj).forEach((key) => {
                 if (key === 'avatar') {
@@ -35,7 +34,52 @@ function renderPage() {
                 }
             })
         })
-        
 }
 
 renderPage()
+
+// Goal 2: Update user avatar
+document.querySelector('.upload').addEventListener('change', (e) => {
+    // 1. Get the selected image file
+    const uploadedFile = e.target.files[0]
+
+    // 2. Create FormData to hold the file and creator info
+    const avatarFormData = new FormData()
+    avatarFormData.append('avatar', uploadedFile)
+    avatarFormData.append('creator', creator)
+
+    // 3. Send PUT request to update avatar on the server
+    axios.put('http://hmajax.itheima.net/api/avatar', avatarFormData
+    ).then((result) => {
+        // 4. Re-render the page to show updated avatar
+        renderPage()
+
+        //     const imgUrl = result.data.data.avatar
+        // // 把新的头像回显到页面上
+        // document.querySelector('.prew').src = imgUrl
+    })
+})
+
+// Goal 3: Update user info
+document.querySelector('.submit').addEventListener('click', () => {
+    const userForm = document.querySelector('.user-form')
+    const userObj = serialize(userForm, { hash: true, empty: true })
+    // console.log(userInfo);
+    userObj.creator = creator
+    // 性别数字字符串，转成数字类型
+    userObj.gender = +userObj.gender
+    console.log(userObj);
+    //{"email":"itheima@itcast.cn","nickname":"itheima","gender":0,"desc":"我是Cooper","creator":"cooper"}
+
+    axios.put('http://hmajax.itheima.net/api/settings',
+        userObj
+    ).then((result) => {
+        // 4.1 创建toast对象
+        const toastDom = document.querySelector('.my-toast')
+        const toast = new bootstrap.Toast(toastDom)
+
+        // 4.2 调用show方法->显示提示框
+        toast.show()
+    })
+
+})
